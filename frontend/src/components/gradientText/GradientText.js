bc291079c06bee08068613f58e805bf261165962
xpextro93 <jonathanandrade@pursuit.org>
2020-12-08 15:28:11 -0500

    add gradient text"

--- frontend/src/components/gradientText/GradientText.js
+++ frontend/src/components/gradientText/GradientText.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import "./gradientText.css"
 
 export default function GradiendText() {
     return(
         <div className="gradient-container">
-            
+            <div className="grad-text">
+                This text is colourful
+            </div>
         </div>
     )
 }
