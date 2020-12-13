3c7727961602259f308d8770a070728ba9c95806
xpextro93 <jonathanandrade@pursuit.org>
2020-12-13 15:34:52 -0500

    add shape css

--- frontend/src/components/threeD/ThreeD.js
+++ frontend/src/components/threeD/ThreeD.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ThreeD.css"
 export default function ThreeD() {
     return(
-        <div> 
+        <div className="3d-container"> 
             <div className="square">
                 <div className="circle"></div>
             </div>
