ce071eaf3166553c34f64c401804915a1a5a8443
xpextro93 <jonathanandrade@pursuit.org>
2020-12-13 15:55:38 -0500

    add 3d circle

--- frontend/src/components/threeD/ThreeD.js
+++ frontend/src/components/threeD/ThreeD.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ThreeD.css"
 export default function ThreeD() {
     return(
-        <div className="3d-container"> 
+        <div className="ddd-container"> 
             <div className="square">
                 <div className="circle"></div>
             </div>
